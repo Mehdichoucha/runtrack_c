7f3745dfd5ce9655a9155f19f9040e5cff6cb697
Linda Choucha <mehdi.choucha@laplateforme.io>
2025-04-09 10:47:16 +0200

    fix some bugs and made ex 3

--- job2/my_strlen.c
+++ job2/my_strlen.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#include <string.h>
 
-int main()
-{
-    char str1[]="12345678";
-    printf("%d",strlen(str1));
+int main(){
+
+    char str[] = "Hello world";
+    int i;
+
+    for (i=0; str[i]!='\0'; ++i);
+    printf("%d", i);
 
     return 0;
 }
+
